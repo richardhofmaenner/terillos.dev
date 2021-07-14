74abebc8291b6e2ce631c928699b1a0158e53064
Richard Hofmänner <hello@terillos.dev>
2021-07-15 01:03:08 +0200

    Added Privacy Policy

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,159 @@
+import PageLayout from "@/layouts/pageLayout";
+
+function Imprint() {
+  return (
+    <PageLayout>
+      <section>
+        <h1>Datenschutzerkl&auml;rung</h1>
+        <p>Verantwortliche Stelle im Sinne der Datenschutzgesetze, insbesondere der
+          EU-Datenschutzgrundverordnung (DSGVO), ist:</p><br/><p>Richard Hofmänner<br/>E-Mail: hello@terillos.dev<br/>WebSite:
+        https://terillos.dev/ </p>
+        <h2>Allgemeiner Hinweis</h2>
+        <p>Gest&uuml;tzt auf Artikel 13 der schweizerischen Bundesverfassung
+          und den datenschutzrechtlichen Bestimmungen des Bundes (Datenschutzgesetz, DSG) hat jede Person
+          Anspruch auf Schutz ihrer Privatsph&auml;re sowie auf Schutz vor Missbrauch ihrer pers&ouml;nlichen Daten. Die
+          Betreiber dieser Seiten nehmen den Schutz Ihrer pers&ouml;nlichen Daten sehr ernst. Wir behandeln Ihre
+          personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser
+          Datenschutzerkl&auml;rung.</p><p>In Zusammenarbeit mit unseren Hosting-Providern bem&uuml;hen wir uns, die
+        Datenbanken so gut wie m&ouml;glich vor fremden Zugriffen, Verlusten, Missbrauch oder vor F&auml;lschung zu
+        sch&uuml;tzen.</p><p>Wir weisen darauf hin, dass die Daten&uuml;bertragung im Internet (z.B. bei der
+        Kommunikation per E-Mail) Sicherheitsl&uuml;cken aufweisen kann. Ein l&uuml;ckenloser Schutz der Daten vor dem
+        Zugriff durch Dritte ist nicht m&ouml;glich.</p><p>Durch die Nutzung dieser Website erkl&auml;ren Sie sich mit
+        der Erhebung, Verarbeitung und Nutzung von Daten gem&auml;ss der nachfolgenden Beschreibung einverstanden. Diese
+        Website kann grunds&auml;tzlich ohne Registrierung besucht werden. Dabei werden Daten wie beispielsweise
+        aufgerufene Seiten bzw. Namen der abgerufenen Datei, Datum und Uhrzeit zu statistischen Zwecken auf dem Server
+        gespeichert, ohne dass diese Daten unmittelbar auf Ihre Person bezogen werden. Personenbezogene Daten,
+        insbesondere Name, Adresse oder E-Mail-Adresse werden soweit m&ouml;glich auf freiwilliger Basis erhoben. Ohne
+        Ihre Einwilligung erfolgt keine Weitergabe der Daten an Dritte.</p><p>&nbsp;</p><h2>Datenschutzerkl&auml;rung
+        f&uuml;r Cookies</h2><p>Diese Website verwendet Cookies. Das sind kleine Textdateien, die es m&ouml;glich
+        machen, auf dem Endger&auml;t des Nutzers spezifische, auf den Nutzer bezogene Informationen zu speichern,
+        w&auml;hrend er die Website nutzt. Cookies erm&ouml;glichen es, insbesondere Nutzungsh&auml;ufigkeit und
+        Nutzeranzahl der Seiten zu ermitteln, Verhaltensweisen der Seitennutzung zu analysieren, aber auch unser Angebot
+        kundenfreundlicher zu gestalten. Cookies bleiben &uuml;ber das Ende einer Browser-Sitzung
+        gespeichert und k&ouml;nnen bei einem erneuten Seitenbesuch wieder aufgerufen werden. Wenn Sie das nicht
+        w&uuml;nschen, sollten Sie Ihren Internetbrowser so einstellen, dass er die Annahme von Cookies verweigert.</p>
+        <p>Ein genereller Widerspruch gegen den Einsatz der zu Zwecken des Onlinemarketing eingesetzten Cookies kann bei
+          einer Vielzahl der Dienste, vor allem im Fall des Trackings, über die US-amerikanische Seite <a
+            href="http://www.aboutads.info/choices/" target="_blank"
+            rel="noopener noreferrer">http://www.aboutads.info/choices/</a> oder die EU-Seite <a
+            href="http://www.youronlinechoices.com/" target="_blank"
+            rel="noopener noreferrer">http://www.youronlinechoices.com/</a> erklärt werden. Des Weiteren kann die
+          Speicherung von
+          Cookies mittels deren Abschaltung in den Einstellungen des Browsers erreicht werden. Bitte beachten Sie, dass
+          dann gegebenenfalls nicht alle Funktionen dieses Onlineangebotes genutzt werden können.</p> <p>&nbsp;</p>
+        <h2>Datenschutzerkl&auml;rung f&uuml;r SSL-/TLS-Verschl&uuml;sselung</h2><p>Diese Website nutzt aus Gr&uuml;nden
+        der Sicherheit und zum Schutz der &Uuml;bertragung vertraulicher Inhalte, wie zum Beispiel der Anfragen, die Sie
+        an uns als Seitenbetreiber senden, eine SSL-/TLS-Verschl&uuml;sselung. Eine verschl&uuml;sselte Verbindung
+        erkennen Sie daran, dass die Adresszeile des Browsers von &quot;http://&quot; auf &quot;https://&quot; wechselt
+        und an dem Schloss-Symbol in Ihrer Browserzeile.</p> <p>Wenn die SSL bzw. TLS Verschl&uuml;sselung aktiviert
+        ist, k&ouml;nnen die Daten, die Sie an uns &uuml;bermitteln, nicht von Dritten mitgelesen werden.</p>
+        <p>&nbsp;</p><h2>Datenschutzerkl&auml;rung f&uuml;r Server-Log-Files</h2><p>Der Provider dieser Website erhebt
+        und speichert automatisch Informationen in so genannten Server-Log Files, die Ihr Browser automatisch an
+        uns &uuml;bermittelt. Dies sind:</p>
+        <ul>
+          <li>Browsertyp und Browserversion</li>
+          <li>verwendetes Betriebssystem</li>
+          <li>Referrer URL</li>
+          <li>Hostname des zugreifenden Rechners</li>
+          <li>Uhrzeit der Serveranfrage</li>
+        </ul>
+        <p>Diese Daten sind nicht bestimmten Personen zuordenbar. Eine Zusammenf&uuml;hrung dieser Daten mit anderen
+          Datenquellen wird nicht vorgenommen. Wir behalten uns vor, diese Daten nachtr&auml;glich zupr&uuml;fen, wenn
+          uns konkrete Anhaltspunkte f&uuml;r eine rechtswidrige Nutzung bekannt werden.</p><p>&nbsp;</p>
+        <h2>Datenschutzerkl&auml;rung f&uuml;r Google Analytics</h2><p>Diese Website benutzt Google Analytics, einen
+        Webanalysedienst der Google Ireland Limited. Wenn der Verantwortliche für die Datenverarbeitung auf dieser
+        Website ausserhalb des Europäischen Wirtschaftsraumes oder der Schweiz sitzt, dann erfolgt die Google Analytics
+        Datenverarbeitung durch Google LLC. Google LLC und Google Ireland Limited werden
+        nachfolgend &laquo;Google&raquo; genannt.</p><p>Über die gewonnenen Statistiken können wir unser Angebot
+        verbessern und für Sie als Nutzer interessanter ausgestalten. Diese Website verwendet Google Analytics zudem für
+        eine geräteübergreifende Analyse von Besucherströmen, die über eine User-ID durchgeführt wird. Sofern Sie über
+        ein Google-Benutzerkonto verfügen, können Sie in den dortigen Einstellungen unter &laquo;Meine
+        Daten&raquo;, &laquo;persönliche Daten&raquo; die geräteübergreifende Analyse Ihrer Nutzung deaktivieren.</p>
+        <p>Rechtsgrundlage für die Nutzung von Google Analytics ist Art. 6 Abs. 1 S. 1 lit. f DS-GVO. Die im Rahmen von
+          Google Analytics von Ihrem Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google
+          zusammengeführt. Wir weisen Sie darauf hin, dass auf dieser Website Google Analytics um den
+          Code &laquo;_anonymizeIp();&raquo; erweitert wurde, um eine anonymisierte Erfassung von IP-Adressen zu
+          gewährleisten. Dadurch werden IP-Adressen gekürzt weiterverarbeitet, eine Personenbeziehbarkeit kann damit
+          ausgeschlossen werden. Soweit den über Sie erhobenen Daten ein Personenbezug zukommt, wird dieser also sofort
+          ausgeschlossen und die personenbezogenen Daten damit umgehend gelöscht.</p> <p>Nur in Ausnahmefällen wird die
+        volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Im Auftrag des Betreibers
+        dieser Website wird Google diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports
+        über die Websitenaktivitäten zusammenzustellen und um weitere mit der Websitennutzung und der Internetnutzung
+        verbundene Dienstleistungen gegenüber dem Websitenbetreiber zu erbringen. Für die Ausnahmefälle, in denen
+        personenbezogene Daten in die USA übertragen werden, hat sich Google dem EU-US Privacy Shield unterworfen, <a
+          href="https://www.privacyshield.gov/EU-US-Framework" target="_blank"
+          rel="noopener noreferrer">https://www.privacyshield.gov/EU-US-Framework</a>.</p> <p>Google Analytics verwendet
+        Cookies.
+        Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website werden in der Regel an einen
+        Server von Google in den USA übertragen und dort gespeichert. Sie können die Speicherung der Cookies durch eine
+        entsprechende Einstellung Ihrer Browser-Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in
+        diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website vollumfänglich werden nutzen können. Sie
+        können darüber hinaus die Erfassung der durch das Cookie erzeugten und auf Ihre Nutzung der Website bezogenen
+        Daten (inkl. Ihrer IP-Adresse) an Google sowie die Verarbeitung dieser Daten durch Google verhindern, indem sie
+        das unter dem folgenden Link verfügbare Browser-Plugin herunterladen und installieren: <a
+          href="https://www.swissanwalt.ch/de/gaoptout.aspx" target="_blank" rel="noopener noreferrer">Google Analytics
+          deaktivieren</a>.</p> <p>Ausserdem können Sie die Nutzung von Google Analytics auch verhindern, indem sie auf
+        diesen Link klicken: <a href="https://tools.google.com/dlpage/gaoptout?hl=de" target="_blank"
+                                rel="noopener noreferrer">Google
+          Analytics deaktivieren</a>. Hierdurch wird ein sog. opt-out Cookie auf ihrem Datenträger gespeichert, der die
+        Verarbeitung personenbezogener Daten durch Google Analytics verhindert. Bitte beachten Sie, dass bei einem
+        Löschen sämtlicher Cookies auf Ihrem Endgerät auch diese Opt-out-Cookies gelöscht werden, d.h., dass Sie erneut
+        die Opt-out-Cookies setzen müssen, wenn Sie weiterhin diese Form der Datenerhebung verhindern wollen. Die
+        Opt-out-Cookies sind pro Browser und Rechner/Endgerät gesetzt und müssen daher für jeden Browser, Rechner oder
+        anderes Endgerät gesondert aktiviert werden.</p><p>&nbsp;</p><h2>Datenschutzerkl&auml;rung für die Nutzung von
+        Google Web Fonts</h2><p>Diese Website nutzt zur einheitlichen Darstellung von Schriftarten so genannte Web
+        Fonts, die von Google bereitgestellt werden. Beim Aufruf einer Seite l&auml;dt Ihr Browser die ben&ouml;tigten
+        Web Fonts in ihren Browsercache, um Texte und Schriftarten korrekt anzuzeigen. Wenn Ihr Browser Web Fonts nicht
+        unterst&uuml;tzt, wird eine Standardschrift von Ihrem Computer genutzt.</p><p>Weitere Informationen zu Google
+        Web Fonts finden Sie unter <a href="https://developers.google.com/fonts/faq" target="_blank"
+                                      rel="noreferrer">https://developers.google.com/fonts/faq</a> und in der
+        Datenschutzerkl&auml;rung von Google: <a href="https://www.google.com/policies/privacy/" target="_blank"
+                                                 rel="noreferrer">https://www.google.com/policies/privacy/</a>
+      </p><p>&nbsp;</p><h2>Google Tag Manager</h2> <p>Google Tag Manager ist eine L&ouml;sung, mit der wir sog.
+        Website-Tags &uuml;ber eine Oberfl&auml;che verwalten k&ouml;nnen und so z.B. Google Analytics sowie andere
+        Google-Marketing-Dienste in unser Onlineangebot einbinden können. Der Tag Manager selbst, welcher die Tags
+        implementiert, verarbeitet keine personenbezogenen Daten der Nutzer. Im Hinblick auf die Verarbeitung der
+        personenbezogenen Daten der Nutzer wird auf die folgenden Angaben zu den Google-Diensten verwiesen.
+        Nutzungsrichtlinien: <a href="https://www.google.com/intl/de/tagmanager/use-policy.html" target="_blank"
+                                rel="noopener noreferrer">https://www.google.com/intl/de/tagmanager/use-policy.html</a>.
+      </p>
+        <p>&nbsp;</p><h2>Hinweis zur Datenweitergabe in die USA</h2> <p>Auf unserer Website sind unter anderem Tools von
+        Unternehmen mit Sitz in den USA eingebunden. Wenn diese Tools aktiv sind, k&ouml;nnen Ihre personenbezogenen
+        Daten an die US-Server der jeweiligen Unternehmen weitergegeben werden. Wir weisen darauf hin, dass die USA kein
+        sicherer Drittstaat im Sinne des EU-Datenschutzrechts sind. US-Unternehmen sind dazu verpflichtet,
+        personenbezogene Daten an Sicherheitsbeh&ouml;rden herauszugeben, ohne dass Sie als Betroffener hiergegen
+        gerichtlich vorgehen k&ouml;nnten. Es kann daher nicht ausgeschlossen werden, dass US-Beh&ouml;rden (z.B.
+        Geheimdienste) Ihre auf US-Servern befindlichen Daten zu &uuml;berwachungszwecken verarbeiten, auswerten und
+        dauerhaft speichern. Wir haben auf diese Verarbeitungst&auml;tigkeiten keinen Einfluss.</p><p>&nbsp;</p>
+        <h2>Allgemeiner Haftungsausschluss</h2> <p>Alle Angaben unseres Internetangebotes wurden sorgf&auml;ltig
+        gepr&uuml;ft. Wir bem&uuml;hen uns, unser Informationsangebot aktuell, inhaltlich richtig und vollst&auml;ndig
+        anzubieten. Trotzdem kann das Auftreten von Fehlern nicht v&ouml;llig ausgeschlossen werden, womit wir keine
+        Garantie f&uuml;r Vollst&auml;ndigkeit, Richtigkeit und Aktualit&auml;t von Informationen auch
+        journalistisch-redaktioneller Art &uuml;bernehmen k&ouml;nnen. Haftungsanspr&uuml;che aus Sch&auml;den
+        materieller oder ideeller Art, die durch die Nutzung der angebotenen Informationen verursacht wurden, sind
+        ausgeschlossen, sofern kein nachweislich vors&auml;tzliches oder grob fahrl&auml;ssiges Verschulden
+        vorliegt.</p><p>Der Herausgeber kann nach eigenem Ermessen und ohne Ank&uuml;ndigung Texte ver&auml;ndern oder
+        l&ouml;schen und ist nicht verpflichtet, Inhalte dieser Website zu aktualisieren. Die Benutzung bzw. der Zugang
+        zu dieser Website geschieht auf eigene Gefahr des Besuchers. Der Herausgeber, seine Auftraggeber oder Partner
+        sind nicht verantwortlich f&uuml;r Sch&auml;den, wie direkte, indirekte, zuf&auml;llige, vorab konkret zu
+        bestimmende oder Folgesch&auml;den, die angeblich durch den Besuch dieser Website entstanden sind
+        und &uuml;bernehmen hierf&uuml;r folglich keine Haftung.</p><p>Der Herausgeber &uuml;bernimmt ebenfalls keine
+        Verantwortung und Haftung f&uuml;r die Inhalte und die Verf&uuml;gbarkeit von Website Dritter, die &uuml;ber
+        externe Links dieser Website erreichbar sind. F&uuml;r den Inhalt der verlinkten Seiten sind ausschliesslich
+        deren Betreiber verantwortlich. Der Herausgeber distanziert sich damit ausdr&uuml;cklich von allen Inhalten
+        Dritter, die m&ouml;glicherweise straf- oder haftungsrechtlich relevant sind oder gegen die guten Sitten
+        verstossen.</p><p>&nbsp;</p><h2>&Auml;nderungen</h2><p>Wir k&ouml;nnen diese Datenschutzerkl&auml;rung jederzeit
+        ohne Vorank&uuml;ndigung anpassen. Es gilt die jeweils aktuelle, auf unserer Website publizierte Fassung. Soweit
+        die Datenschutzerkl&auml;rung Teil einer Vereinbarung mit Ihnen ist, werden wir Sie im Falle einer
+        Aktualisierung über die &Auml;nderung per E-Mail oder auf andere geeignete Weise informieren.</p><p>&nbsp;</p>
+        <h2>Fragen an den Datenschutzbeauftragten</h2> <p>Wenn Sie Fragen zum Datenschutz haben, schreiben Sie uns bitte
+        eine E-Mail oder wenden Sie sich direkt an die f&uuml;r den Datenschutz zu Beginn der Datenschutzerkl&auml;rung
+        aufgef&uuml;hrten, verantwortlichen Person in unserer Organisation.</p><p>&nbsp;</p><p>Frauenfeld,
+        15.07.2021<br/>Quelle: <a href="https://www.swissanwalt.ch" target="_blank"
+                                  rel="noopener noreferrer">SwissAnwalt</a></p>
+      </section>
+    </PageLayout>
+  );
+}
+
+export default Imprint;
